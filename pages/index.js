48610fc94bb882d3338bcc5e8f5a186b9081c666
Yuri Tofano <120397249+oYuriTM@users.noreply.github.com>
2025-12-20 00:08:53 +0000

    Adicionando scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,15 @@
-function Home(){
-return <h1>Sabia que metade da salsicha é só sal? Porque o resto é "sicha"</h1>
+function Home() {
+  return (
+    <h1>Sabia que metade da salsicha é só sal? Porque o resto é "sicha"</h1>
+  );
+}
+
+function Teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("indentação errada");
 }
 
 export default Home;
